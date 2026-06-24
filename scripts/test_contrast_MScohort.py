9d41176d167896afc391462816cf17b86eaf187b
Mathilde Ripart <mathilde.ripart@kcl.ac.uk>
2026-06-24 16:00:45 +0100

    clean script to test on MS cohort

--- scripts/test_contrast_MScohort.py
+++ scripts/test_contrast_MScohort.py
@@ -1,69 +1,65 @@
-# %%
+''' Script to test contrast detection on the open_ms_data (https://github.com/muschellij2/open_ms_data)
+    Download the dataset using:
+    git clone https://github.com/muschellij2/open_ms_data.git
+'''
+
+#  %%
 import os
-import json
-import argparse
 import nibabel as nib
 import pandas as pd
 from clinmriqc.contrast import detect_contrast_enhancement
+ 
+
+#%% 
+# initialise path to open_ms_data
+folder = '/Users/mathilderipart/Documents/work/260624_BMEIS_hackathon/open_ms_data/cross_sectional/coregistered'
+
+# find subjects to run on. Here we look at all patients in the coregistered folder
+subjects = os.listdir(folder)
+subjects = [subject for subject in subjects if 'patient' in subject]
 
 # %%
-def main():
-    # %%
-    parser = argparse.ArgumentParser(description="ClinMRI-QC: run full QC pipeline")
-    parser.add_argument("--image", required=True, help="Path to image")
-    parser.add_argument("--brain_mask", required=True, help="Path to brain mask NIfTI image")
-    parser.add_argument("--outfile", default=None, help="Optional path to save JSON results")
-    args = parser.parse_args()
+# for each patient, find the T1w image with contrast (T1WKS.nii.gz) and without (T1W.nii.gz) and the brain mask (brainmask.nii.gz)
+# apply the function to assess if contrast is present
+# TODO: get the brain mask from the image given
 
-    image = args.image
-    brain_mask = args.brain_mask
-    outfile = args.outfile
-    
+df = pd.DataFrame()
+for subject in subjects:
+    for contrast in [False, True]:
+        if contrast:
+            image = os.path.join(folder, subject, 'T1WKS.nii.gz')
+        else:
+            image = os.path.join(folder, subject, 'T1W.nii.gz')
+        brain_mask = os.path.join(folder, subject, 'brainmask.nii.gz')
+        
+        # load nifti file
+        image_arr = nib.load(image).get_fdata()
+        mask_arr = nib.load(brain_mask).get_fdata().astype(bool)
 
-    # %% TEST
-    folder = '/Users/mathilderipart/Documents/work/260624_BMEIS_hackathon/open_ms_data/cross_sectional/coregistered'
-    subjects = os.listdir(folder)
-    subjects = [subject for subject in subjects if 'patient' in subject]
-    # %%
-    
-    df = pd.DataFrame()
-    for subject in subjects:
-        for contrast in [False, True]:
-            if contrast:
-                image = os.path.join(folder, subject, 'T1WKS.nii.gz')
-            else:
-                image = os.path.join(folder, subject, 'T1W.nii.gz')
-            brain_mask = os.path.join(folder, subject, 'brainmask.nii.gz')
-            outfile = None
-            
-            image_arr = nib.load(image).get_fdata()
-            mask_arr = nib.load(brain_mask).get_fdata().astype(bool)
+        # detect contrast
+        results = detect_contrast_enhancement(image_arr, 
+                                                mask_arr, 
+                                                vessel_ratio_threshold = 1.6, 
+                                                bright_fraction_threshold = 0.002)
 
-            results = detect_contrast_enhancement(image_arr, 
-                                                  mask_arr, 
-                                                  vessel_ratio_threshold = 1.6, 
-                                                  bright_fraction_threshold = 0.002)
+        # write results in dataframe
+        results['id'] = subject
+        results['path'] = image
+        results['contrast'] = contrast
 
-            # results
-            results['id'] = subject
-            results['path'] = image
-            results['contrast'] = contrast
+        df = pd.concat([df, pd.DataFrame([results])])
 
-            df = pd.concat([df, pd.DataFrame([results])])
+# analyse results
+df_group = df.groupby(['contrast', 'enhanced'])
 
-    # analyse results
-
-    df_group = df.groupby(['contrast', 'enhanced'])
-
-    out = pd.DataFrame({
-    'count': df_group['id'].count(),
-    'vessel_ratio_mean': df_group['vessel_ratio'].mean(),
-    'vessel_ratio_min': df_group['vessel_ratio'].min(),
-    'vessel_ratio_max': df_group['vessel_ratio'].max(),
-    'bright_voxel_fraction_mean': df_group['bright_voxel_fraction'].mean(),
-    'bright_voxel_fraction_min': df_group['bright_voxel_fraction'].min(),
-    'bright_voxel_fraction_max': df_group['bright_voxel_fraction'].max(),
-    })
-    out
+out = pd.DataFrame({
+'count': df_group['id'].count(),
+'vessel_ratio_mean': df_group['vessel_ratio'].mean(),
+'vessel_ratio_min': df_group['vessel_ratio'].min(),
+'vessel_ratio_max': df_group['vessel_ratio'].max(),
+'bright_voxel_fraction_mean': df_group['bright_voxel_fraction'].mean(),
+'bright_voxel_fraction_min': df_group['bright_voxel_fraction'].min(),
+'bright_voxel_fraction_max': df_group['bright_voxel_fraction'].max(),
+})
 
 # %%
